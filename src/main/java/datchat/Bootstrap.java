e7dbbcd8e93645df591c0d0d15d7ac66be4f8388
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-08-10 23:27:40 +0300

    Initial verticles and build modifications

---
+++ src/main/java/datchat/Bootstrap.java
@@ -0,0 +1,36 @@
+package datchat;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class Bootstrap extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        Router router = Router.router(vertx);
+
+        createRoutes(router);
+
+        vertx.createHttpServer()
+                .requestHandler(router::accept)
+                .listen(8080, result -> {
+                    if (result.succeeded()) {
+                        startFuture.complete();
+                    } else {
+                        startFuture.fail(result.cause());
+                    }
+                });
+    }
+
+    private void createRoutes(Router router) {
+        router.get("/").handler(StaticHandler.create("assets"));
+
+        router.get("/api/healthcheck").handler(c ->
+                c.response()
+                        .setStatusCode(200)
+                        .end("Hello, world!")
+        );
+    }
+}
