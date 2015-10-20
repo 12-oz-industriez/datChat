20bc8f5d5724d4c2fe31620e849b5a5ce697c021
Oleksii Bieliaiev <4n41nd@gmail.com>
2015-10-21 01:01:00 +0300

    removed crap

--- src/main/resources/assets/js/messages/sendMessage.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by anaind on 10/4/15.
- */
-//Add event listeners for send button
-/*
-var button = document.getElementById($.datChat.CONSTANTS.SEND_MESSAGE_BUTTON_ID );
-button.addEventListener()
-button.addEventListener($.datChat.sendChatMessage($.datChat.sendChatMessage()))*/
