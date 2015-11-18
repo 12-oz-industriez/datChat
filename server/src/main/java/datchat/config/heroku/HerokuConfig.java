03f177f345d6bcd22a2a72e16e48570a8026728b
Aleksandr Oksenenko <crew4ok@gmail.com>
2015-11-18 23:12:17 +0200

    Fix heroku deployment

--- server/src/main/java/datchat/config/heroku/HerokuConfig.java
+++ server/src/main/java/datchat/config/heroku/HerokuConfig.java
@@ -2,32 +2,17 @@ package datchat.config.heroku;
 
 import com.mongodb.async.client.MongoClient;
 import com.mongodb.async.client.MongoClients;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
-import io.vertx.redis.RedisClient;
-import io.vertx.redis.RedisOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import javax.inject.Inject;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.concurrent.CompletableFuture;
-
 @Configuration
 @Profile("heroku")
 public class HerokuConfig {
 
-    @Inject
-    private Vertx vertx;
-
     @Value("#{systemEnvironment['MONGOLAB_URI']}")
     private String mongoConnectionString;
-
-    @Value("#{systemEnvironment['REDIS_URL']}")
-    private String redisConnectionString;
 
     @Value("#{systemEnvironment['PORT']}")
     private Integer port;
@@ -35,23 +20,6 @@ public class HerokuConfig {
     @Bean
     public MongoClient mongoClient() {
         return MongoClients.create(mongoConnectionString);
-    }
-
-    @Bean
-    public RedisClient redisClient() {
-        try {
-            URI redisURI = new URI(redisConnectionString);
-
-            String password = redisURI.getUserInfo().split(":")[1];
-            RedisOptions redisOptions = new RedisOptions()
-                    .setHost(redisURI.getHost())
-                    .setPort(redisURI.getPort())
-                    .setAuth(password);
-
-            return RedisClient.create(vertx, redisOptions);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Bean(name = "port")
